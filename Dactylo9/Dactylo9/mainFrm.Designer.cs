ff82363508a14c497c62bec32ecd091b828f4047
Senahk <michael-arian.rms@eduge.ch>
2017-03-02 13:55:38 +0100

    creation View (forms)

---
+++ Dactylo9/Dactylo9/mainFrm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Dactylo9
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxTextSample = new System.Windows.Forms.TextBox();
+            this.lblF = new System.Windows.Forms.Label();
+            this.lblT = new System.Windows.Forms.Label();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxTextSample
+            // 
+            this.tbxTextSample.Location = new System.Drawing.Point(24, 12);
+            this.tbxTextSample.Multiline = true;
+            this.tbxTextSample.Name = "tbxTextSample";
+            this.tbxTextSample.ReadOnly = true;
+            this.tbxTextSample.Size = new System.Drawing.Size(563, 278);
+            this.tbxTextSample.TabIndex = 0;
+            // 
+            // lblF
+            // 
+            this.lblF.AutoSize = true;
+            this.lblF.Location = new System.Drawing.Point(31, 308);
+            this.lblF.Name = "lblF";
+            this.lblF.Size = new System.Drawing.Size(48, 13);
+            this.lblF.TabIndex = 1;
+            this.lblF.Text = "Fautes : ";
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Location = new System.Drawing.Point(395, 308);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(80, 13);
+            this.lblT.TabIndex = 2;
+            this.lblT.Text = "Temps écoulé :";
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(24, 344);
+            this.tbxInput.Multiline = true;
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(347, 220);
+            this.tbxInput.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(392, 344);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(59, 47);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "1\r\n,.!?";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(457, 344);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(59, 47);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "2\r\nABC";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(522, 344);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(59, 47);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "3\r\nDEF";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(522, 402);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(59, 47);
+            this.btn6.TabIndex = 9;
+            this.btn6.Text = "6\r\nMNO";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(457, 402);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(59, 47);
+            this.btn5.TabIndex = 8;
+            this.btn5.Text = "5\r\nJKL";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(392, 402);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(59, 47);
+            this.btn4.TabIndex = 7;
+            this.btn4.Text = "4\r\nGHI";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(522, 464);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(59, 47);
+            this.btn9.TabIndex = 12;
+            this.btn9.Text = "9\r\nWXYZ";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(457, 464);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(59, 47);
+            this.btn8.TabIndex = 11;
+            this.btn8.Text = "8\r\nTUV";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(392, 464);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(59, 47);
+            this.btn7.TabIndex = 10;
+            this.btn7.Text = "7\r\nPQRS";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(457, 517);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(59, 47);
+            this.btn10.TabIndex = 13;
+            this.btn10.Text = "0\r\n␣";
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 589);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblF);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.tbxTextSample);
+            this.Name = "mainFrm";
+            this.Text = "Dactylo9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxTextSample;
+        private System.Windows.Forms.Label lblF;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn10;
+    }
+}
+
--- Dactylo9/Dactylo9/mainFrm.cs
+++ Dactylo9/Dactylo9/mainFrm.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace Dactylo9
 {
-    public partial class Form1 : Form
+    public partial class mainFrm : Form
     {
-        public Form1()
+        public mainFrm()
         {
             InitializeComponent();
         }
+
+
     }
 }
